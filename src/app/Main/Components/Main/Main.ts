6ba5588502022491bcf0ff74a0319917bd491695
thinkingmedia <mathew@thinkingmedia.ca>
2018-01-18 11:07:22 -0500

    fixed collapsing of the navbar on mobile

--- src/app/Main/Components/Main/Main.ts
+++ src/app/Main/Components/Main/Main.ts
@@ -1,4 +1,6 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnDestroy} from '@angular/core';
+import {NavigationStart, Router} from '@angular/router';
+import {SubscriptionMap} from '../../../Shared/Utils/SubscriptionMap';
 
 @Component({
     selector: 'app-main',
@@ -6,5 +8,32 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
     styleUrls: ['./Main.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MainComponent {
+export class MainComponent implements OnDestroy {
+    /**
+     * Handles the collapsing of the navbar on mobile.
+     */
+    public toggleCollapse: boolean = false;
+
+    /**
+     * Helper for subscriptions.
+     */
+    private unsub: SubscriptionMap = new SubscriptionMap();
+
+    /**
+     * Constructor
+     */
+    public constructor(router: Router) {
+        this.unsub.set('router', router.events.subscribe((event) => {
+            if (event instanceof NavigationStart) {
+                this.toggleCollapse = false;
+            }
+        }));
+    }
+
+    /**
+     * Unsubscribe
+     */
+    public ngOnDestroy(): void {
+        this.unsub.unsubscribe();
+    }
 }
